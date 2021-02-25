98e80d6080a795300afa71653cd2e4232564419d
Dipesh Dhakal <dipesh@frollo.us>
2021-02-25 17:53:54 +1100

    Initial Commit

---
+++ Kuljam SwaroopTests/Kuljam_SwaroopTests.swift
@@ -0,0 +1,34 @@
+//
+//  Kuljam_SwaroopTests.swift
+//  Kuljam SwaroopTests
+//
+//  Created by Dipesh Dhakal on 25/2/21.
+//  Copyright © 2021 Nijanand. All rights reserved.
+//
+
+import XCTest
+@testable import Kuljam_Swaroop
+
+class Kuljam_SwaroopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
